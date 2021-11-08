2da0d83c1724194f253d0b54791a3fcbf17d80d9
txemac <txema.bermudez@gmail.com>
2021-11-08 21:45:25 +0100

    add test with q empty

--- test/product/infrastructure/views/test_product_views.py
+++ test/product/infrastructure/views/test_product_views.py
@@ -12,3 +12,12 @@ def test_get_garments_without_q(
     response = client.get("/garments/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == [new_garment.dict(), new_garment.dict(), new_garment.dict()]
+
+
+def test_get_garments_with_q_empty(
+    client: TestClient,
+    new_garment: Garment,
+) -> None:
+    response = client.get("/garments/?q=")
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+    assert response.json()["detail"][0]["msg"] == "ensure this value has at least 1 characters"
