ffb2441806c2ea6f37c9760163dbd8a43070a7ab
txemac <txema.bermudez@gmail.com>
2021-11-08 18:03:01 +0100

    include routes at create app

--- src/main.py
+++ src/main.py
@@ -3,11 +3,15 @@ from typing import Dict
 import uvicorn
 from fastapi import FastAPI
 
+from product.infrastructure.views.product_views import api_products
+
 
 def create_app() -> FastAPI:
     api = FastAPI(
         title="intelistyle API",
     )
+
+    api.include_router(api_products, prefix='/products', tags=['Products'])
 
     return api
 
